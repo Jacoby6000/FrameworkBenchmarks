256595925472944c52ccd76012eeb1df1b1cfaea
Emily Pillmore <emilyp@cakesolutions.net>
2018-03-18 23:52:07 -0400

    remove unnecessary implicits

--- frameworks/Scala/http4s/src/main/scala/http4s/techempower/benchmark/http/DatabaseHttpEndpoint.scala
+++ frameworks/Scala/http4s/src/main/scala/http4s/techempower/benchmark/http/DatabaseHttpEndpoint.scala
@@ -1,21 +1,22 @@
 package http4s.techempower.benchmark.http
 
-import cats.{Monad, Show}
-import cats.implicits._
+import cats.Monad
 import cats.effect.Effect
-import http4s.techempower.benchmark.service.DatabaseService
+import cats.implicits._
 import http4s.techempower.benchmark.implicits._
-import org.http4s.{HttpService, MediaType, implicits}
+import http4s.techempower.benchmark.service.DatabaseService
 import org.http4s.dsl.Http4sDsl
 import org.http4s.headers.`Content-Type`
+import org.http4s.{HttpService, MediaType}
+
 
 final class DatabaseHttpEndpoint[F[_]: Effect](
     databaseService: DatabaseService[F]) {
 
   def service(implicit F: Monad[F]): HttpService[F] = {
     val dsl: Http4sDsl[F] = new Http4sDsl[F] {}
-    import dsl._
     import databaseService._
+    import dsl._
     HttpService[F] {
       case GET -> Root / "db" =>
         for {
